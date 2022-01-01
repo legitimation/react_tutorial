12bca1ac236930fb68ee30315b11113689b78735
Zieho Choi <jeff426@naver.com>
2022-01-01 19:16:27 +0900

    9 intro of custom hooks (usefetch imporved)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Setup from './tutorial/8-useContext/final/1-context-api'
+import Setup from './tutorial/9-custom-hooks/final/1-fetch-example'
 import Trial from './tutorial/1-useState/setup/2-useState-basics'
 
 function App() {
--- src/tutorial/9-custom-hooks/final/2-useFetch.js
+++ src/tutorial/9-custom-hooks/final/2-useFetch.js
@@ -1,5 +1,10 @@
 import { useState, useEffect, useCallback } from 'react';
 
+
+// custom hook is using hook-like features made individually (not provided from react)
+// can be used in ur own goods (can be altered)
+// similar to making a seperate component of a func. that will be used multiple times throughout the project
+
 export const useFetch = (url) => {
   const [loading, setLoading] = useState(true);
   const [products, setProducts] = useState([]);
