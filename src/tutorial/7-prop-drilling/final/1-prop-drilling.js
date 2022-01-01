56369704b054a7a6e04b26ebeeaf1da3714c8fb7
Zieho Choi <jeff426@naver.com>
2022-01-01 18:32:38 +0900

    7 -1 prop drilling, sending props til bottom (y we need redux)

--- src/tutorial/7-prop-drilling/final/1-prop-drilling.js
+++ src/tutorial/7-prop-drilling/final/1-prop-drilling.js
@@ -3,6 +3,10 @@ import { data } from '../../../data';
 // more components
 // fix - context api, redux (for more complex cases)
 
+
+// at the very top component, function removeperson is defined, in this case it should be sent as props til the very end where removeperson actually takes place
+// however this is inefficient as the inner function does not require func removeperson, thus explains the reason why state libraries like redux, mobx are needed and used
+
 const PropDrilling = () => {
   const [people, setPeople] = useState(data);
   const removePerson = (id) => {
